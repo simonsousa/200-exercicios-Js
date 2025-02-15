3082f9a97b587f7b516e83a7c9206e7bcf805965
simonsousa <simonsousa335@gmail.com>
2025-02-15 15:40:58 -0300

    Ex031

---
+++ Ex031.js
@@ -0,0 +1,6 @@
+/*
+    Usando um laço while, crie um programa que adivinha um número que o usuário pensou através do método de busca binária
+
+    Neste exercício, você vai simular um jogo onde o usuário "pensa" em um número de 1 a 100 e o programa deve adivinhar o número usando o método de busca binária. Para simplificar, vamos super que o número pensado pelo usuário é 50.
+    
+*/
